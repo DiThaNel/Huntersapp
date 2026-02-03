b84c932770d195598f3af53553230e58643c4142
Gabriel Goncalves <desarrolloweb@artsolut.es>
2026-02-03 11:27:21 +0000

    Gpages

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/Huntersapp",
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
