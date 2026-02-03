e98d83cded3808a0a1dd777d8e846f3a25b5d4a9
Gabriel Goncalves <desarrolloweb@artsolut.es>
2026-02-03 11:32:10 +0000

    GPages Back

--- app/api/characters/route.ts
+++ app/api/characters/route.ts
@@ -1,8 +1,6 @@
 import { NextResponse } from 'next/server';
 import { Hunters } from '@/data/characters';
 
-export const dynamic = 'force-static';
-
 export async function GET() {
   return NextResponse.json(Hunters);
 }
