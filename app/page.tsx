c3a4ef15676f93c22e4a2361996aa2cf3d04d725
Gabriel Goncalves <desarrolloweb@artsolut.es>
2026-02-08 19:01:46 +0000

    Update background to gradient

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import CharacterList from "./components/CharacterList";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-zinc-50 dark:bg-white">
+    <div className="min-h-screen bg-gradient-to-b from-[#fdfdfd] to-[#d4d4d4]/10 opacity-90">
       <main className="flex min-h-screen flex-col items-center justify-between p-4 sm:p-8">
         <CharacterList characters={Hunters} />
       </main>
